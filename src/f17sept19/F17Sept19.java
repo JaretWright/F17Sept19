ba7e2271a403370e1a4e7f5523596758d77a045a
JaretWright <hkkA223>
2017-09-19 14:45:03 -0400

    removed a comment

---
+++ src/f17sept19/F17Sept19.java
@@ -0,0 +1,16 @@
+package f17sept19;
+
+/**
+ *
+ * @author JWright
+ */
+public class F17Sept19 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
