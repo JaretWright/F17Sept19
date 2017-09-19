7026b22bbcbf6be15f23590f45ccc9d9f695ffa1
JaretWright <hkkA223>
2017-09-19 15:12:40 -0400

    Added the feature of getting someone's name

--- src/f17sept19/F17Sept19.java
+++ src/f17sept19/F17Sept19.java
@@ -1,5 +1,8 @@
 package f17sept19;
 
+import java.util.Scanner;
+
+
 /**
  *
  * @author JWright
@@ -11,6 +14,18 @@ public class F17Sept19 {
      */
     public static void main(String[] args) {
         
+        String myName;  //this reserves space in the computers' memory to store a String
+                        //This String can be referenced by using the variable name myName
+                        
+        System.out.print("What is your name? ");
+        
+        //We need a way to read what the user types into the keyboard, so we instantiate 
+        //a Scanner object
+        Scanner keyboard = new Scanner(System.in);
+
+        myName = keyboard.nextLine();
+        
+        System.out.printf("Glad to meet you %s%n", myName);
     }
     
 }
